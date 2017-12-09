786760d13231984ae7e6138f27fdfba0c0298c89
PC219-10\NTUT <jvcsuy@gmail.com>
2017-12-09 10:44:10 +0800

    third commit

--- src/sourcetreetest/SourceTreeJavaTest.java
+++ src/sourcetreetest/SourceTreeJavaTest.java
@@ -3,8 +3,12 @@ package sourcetreetest;
 public class SourceTreeJavaTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int x = 0;
+		
+		System.out.println(x);
+		
+		
 	}
 
 }
